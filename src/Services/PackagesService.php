0fbda583f343a690d031df7283d652badf3ad31b
kminanov <kiril.minanov@airalo.com>
2026-01-28 14:58:53 +0100

    feat: Update SDKs to include throttling in flattened mode (AP-7791)

--- src/Services/PackagesService.php
+++ src/Services/PackagesService.php
@@ -154,7 +154,7 @@ class PackagesService
                         'is_unlimited' => $package['is_unlimited'],
                         'title' => $package['title'],
                         'is_fair_usage_policy' => $package['is_fair_usage_policy'],
-                        "fair_usage_policy" => $package['fair_usage_policy'] ?? null,
+                        'fair_usage_policy' => $package['fair_usage_policy'] ?? null,
                         'data' => $package['data'],
                         'short_info' => $package['short_info'],
                         'voice' => $package['voice'],
